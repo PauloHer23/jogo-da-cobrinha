de767d4964b9c4e025329d12a6c62c9f69a10f6c
PauloHer23 <paulo.her2100@gmail.com>
2021-03-24 17:24:39 -0300

    criar o lanchinho da cobra

--- script_cobra.js
+++ script_cobra.js
@@ -1,30 +1,37 @@
-let canvas=document.getElementById("Snake");
-let context=canvas.getContext("2d");
-let box=32;
-let snake=[];
-snake[0]={
-    x:8*box,
-    y:8*box
+let canvas = document.getElementById("Snake");
+let context = canvas.getContext("2d");
+let box = 32;
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
 }
-let direction="right";
-
+let direction = "right";
+let lanche = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 function criarBG(){
     context.fillStyle="lightgreen";
-    context.fillRect(0,0,16 * box,16 * box);
+    context.fillRect(0, 0, 16 * box,16 * box);
 }
 function criarCobrinha(){
-    for(i=0;i<snake.length;i++){
-        context.fillStyle="green";
+    for(i = 0; i < snake.length; i++){
+        context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
+}
+function desenharLanche(){
+    context.fillStyle = "red";
+    context.fillRect(lanche.x, lanche.y, box, box)
 }
 document.addEventListener('keydown',update);
 
 function update(event){
-    if(event.keyCode == 37 && direction!="right") direction= "left";
-    if(event.keyCode == 38 && direction!="down") direction= "up";
-    if(event.keyCode == 39 && direction!="left") direction="right";
-    if(event.keyCode == 40 && direction!="up") direction="down";
+    if(event.keyCode == 37 && direction!="right") direction = "left";
+    if(event.keyCode == 38 && direction!="down") direction = "up";
+    if(event.keyCode == 39 && direction!="left") direction = "right";
+    if(event.keyCode == 40 && direction!="up") direction = "down";
     
 }
 function iniciarJogo(){
@@ -36,12 +43,13 @@ function iniciarJogo(){
 
     criarBG();
     criarCobrinha();
+    desenharLanche();
     let snakeX=snake[0].x;
     let snakeY=snake[0].y;
-    if(direction=="right") snakeX+=box;
-    if(direction=="left") snakeX-=box;
-    if(direction=="up") snakeY-=box;
-    if(direction=="down") snakeY+=box;
+    if(direction=="right") snakeX += box;
+    if(direction=="left") snakeX -= box;
+    if(direction=="up") snakeY -= box;
+    if(direction=="down") snakeY += box;
 
     snake.pop();
 
